08bc0ee0e419febd35291b557d64ec1f1ca97832
“ZUMRAN <zumran.nain@flybits.com>
2022-02-07 09:00:47 -0500

    first commit

---
+++ Concentration2/Card.swift
@@ -0,0 +1,36 @@
+//
+//  Card.swift
+//  Concentration2
+//
+//  Created by Zumran Nain on 2022-01-18.
+//
+
+
+import Foundation
+
+struct Card: Hashable {
+    
+    var hashValue: Int {
+        return identifier
+    }
+    
+    static func ==(lhs: Card, rhs: Card) -> Bool {
+        return lhs.identifier == rhs.identifier
+    }
+    
+    
+    var isFaceUp = false
+    var isMatched = false
+    private var identifier: Int
+    
+    private static var identifierFactory = 0
+    
+    private static func getUniqueIdentifier() -> Int {
+        identifierFactory += 1
+        return identifierFactory
+    }
+    
+    init() {
+        self.identifier = Card.getUniqueIdentifier()
+    }
+}
